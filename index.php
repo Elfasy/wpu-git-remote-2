bc683cbddd9a6d446a9c819603bd45d1790eda49
elfasy <affily.ajat@gmail.com>
2019-07-07 21:27:13 +0700

    menambah paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi tempore nesciunt nemo ipsum accusantium veritatis explicabo voluptate, neque, hic fugit itaque soluta maxime! Dolore quis, blanditiis optio facilis natus laborum.</p>
 </body>
 </html>
